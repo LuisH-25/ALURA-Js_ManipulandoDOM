584afdefb80ba0da39b7306b9fd86bc2e46ad646
LuisH-25 <huanca.luis@pucp.pe>
2023-06-05 20:25:25 -0500

    añadiendo arrow function

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
-# Instalamos Live Server
+# Descripcion
+Se esta realizando un aplicativo donde lista, elimina y coloca un tag de completado a tareas que se agregan en un formulario
 
-# abrir liove server
+# Pasos realizados
+## Instalamos Live Server
+
+## abrir live server
 click derecho a index.html
 
-# Buscar y reemplazar un texto
+## Buscar y reemplazar un texto
 document.querySelector("li")
 document.querySelector("span").textContent
 document.querySelector("span").textContent="Aprendder"
--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-document.querySelector();
+const btn = document.querySelector('[data-form-btn]');
+
+//Arrow functions o funciones anonimas
+const createTask=(evento) =>{
+  evento.preventDefault();      //evita que recargue la pagina
+  const input = document.querySelector("[data-form-input]")
+  console.log(input.value);
+}  
+
+btn.addEventListener("click", createTask)
+
+//Arrow functions o funciones anonimas
+// btn.addEventListener("click",(evento) =>{
+//   evento.preventDefault();      //evita que recargue la pagina
+//   const input = document.querySelector("[data-form-input]")
+//   console.log(input.value);
+// })  
+
+
+
